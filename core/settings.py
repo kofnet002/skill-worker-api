fea2cc029dc02ac6591edb2a5674cc3a41d5b192
frimpong-kofi <noeljoel61@gmail.com>
2023-03-06 12:26:54 +0000

    Modify the model file  + Add User model  + Add Worker model  + Add client model
    Made settings to allow picture upload and storage
    Add nem empty file, otp.py to handle the OTP using Twilio

--- core/settings.py
+++ core/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'skillworkerapi.User'
+
+MEDIA_ROOT =  BASE_DIR / 'static/images'
+MEDIA_URL = '/images/'
--- skillworkerapi/models.py
+++ skillworkerapi/models.py
@@ -3,14 +3,53 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    is_client = models.BooleanField(default=False)
+    is_worker = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.username
+
+
+class Client(models.Model):
     gender = (
         ("Male", "Male"),
         ("Female", "Female"),
     )
+
+    occupation = (
+        ("Student", "Student"),
+        ("Lecturer", "Lecturer"),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    name = models.CharField(max_length=250, blank=True, null=True)
+    age = models.IntegerField(null=True)
+    gender = models.CharField(choices=gender, max_length=20, null=True)
+    occupation = models.CharField(choices=occupation, max_length=20, null=True)
+    phone_number = models.CharField(max_length=12, null=True)
+    location = models.CharField(max_length=250, null=True)
+    id_number = models.CharField(max_length=250, null=True)
+    pic = models.ImageField(null=True, default='net.jpg')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # status = models.BooleanField(default=True, null=True)
+    # role_id
+    # skill_id
+
+class Worker(models.Model):
+    gender = (
+    ("Male", "Male"),
+    ("Female", "Female"),
+    )
+
     skill = (
         ("Plumbing", "Plumbing"),
         ("Carpentry", "Carpentry"),
     )
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
     name = models.CharField(max_length=250, blank=True, null=True)
     age = models.IntegerField(null=True)
     gender = models.CharField(choices=gender, max_length=20, null=True)
@@ -18,14 +57,10 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=12, null=True)
     location = models.CharField(max_length=250, null=True)
     id_number = models.CharField(max_length=250, null=True)
-    
-    # status = models.BooleanField(default=True, null=True)
-    # role_id
-    # skill_id
-
+    pic = models.ImageField(null=True, default='net.jpg')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-
-    def __str__(self):
-        return self.username
+    # status = models.BooleanField(default=True, null=True)
+    # role_id
+    # skill_id
